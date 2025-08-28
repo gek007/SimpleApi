aa1114341aab1d01a0cb0e18b986331c6edd5131
kostya shilkrot <kostya.shilkrot@gmail.com>
2025-08-28 14:03:34 +0300

    added simple api

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"msg":"Hello Kostya"}
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
