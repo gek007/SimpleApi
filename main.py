b20ab70aa094139d2a4d014a4c17bc423bb1f6d7
kostya shilkrot <kostya.shilkrot@gmail.com>
2025-08-28 19:03:40 +0300

    filleed docker - composed

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ def root():
     return {"msg":"Hello Kostya"}
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+# if __name__ == '__main__':
+#     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
